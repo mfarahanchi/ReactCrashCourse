ab436fccbcbcdf543a9a3ce171fd9e0dc384f244
Mohsen Farahanchi <mohsenfarahanchi@outlook.com>
2019-06-23 18:04:01 +0500

    pass argument to event handler solution2 - min 76'

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -27,16 +27,14 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 });
   };
 
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
-  };
-
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.doHandleIncrement}
+          onClick={() => {
+            this.handleIncrement({ id: 1 });
+          }}
           className="btn btn-secondary btn-sm"
         >
           Increment
