1b480767404a1946b48206785c10e19f3ede9c40
Mohsen Farahanchi <mohsenfarahanchi@outlook.com>
2019-06-23 20:10:37 +0500

    add nav bar min 120'

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          React Crash Course
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
