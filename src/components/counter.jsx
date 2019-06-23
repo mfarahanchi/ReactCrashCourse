0c805380ac9229db3b054f3547a9f5b52446acde
Mohsen Farahanchi <mohsenfarahanchi@outlook.com>
2019-06-23 17:18:49 +0500

    styling min 47'

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,18 +4,30 @@ class Counter extends Component {
   state = {
     count: 0
   };
+  styles = {
+    fontFamily: "Verdana",
+    fontWeight: "bold",
+    fontSize: 15
+  };
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button
+          style={{ fontStyle: "italic" }}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
   }
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
